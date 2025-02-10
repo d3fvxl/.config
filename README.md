59b22a5ae572281eb1aad671faf9fdc2548b15d5
defval <m.bovtunov@efg.gg>
2025-02-10 19:20:31 +0300

    add copilot, peek, readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# .config
+
+This is my personal configuration files for my OS X setup.
+
+![terminal screenshot](./config.png)
---
+++ nvim/lua/plugins/peek.lua
@@ -0,0 +1,13 @@
+return {
+    "saimo/peek.nvim",
+    build = "deno task --quiet build:fast",
+    config = function()
+        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+    cmd = {
+        "PeekOpen",
+        "PeekClose",
+    },
+}
+
